dcb3c0d5dde6532df30e3858e0bbf1b880dd797d
harshvaghanii <harshvaghani98@gmail.com>
2025-01-24 19:53:24 -0600

    initial commit

---
+++ src/main/java/com/vaghani/project/ridesharing/ridesharingapp/RidesharingappApplication.java
@@ -0,0 +1,13 @@
+package com.vaghani.project.ridesharing.ridesharingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidesharingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidesharingappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaghani/project/ridesharing/ridesharingapp/RidesharingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaghani.project.ridesharing.ridesharingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidesharingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
