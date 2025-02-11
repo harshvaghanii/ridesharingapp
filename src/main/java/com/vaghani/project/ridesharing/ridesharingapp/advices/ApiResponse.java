ecdf4965bf128db110e15b088680cbb25733b14a
harshvaghanii <harshvaghani98@gmail.com>
2025-02-10 22:14:45 -0600

    removing unwanted tests and imports

--- src/main/java/com/vaghani/project/ridesharing/ridesharingapp/advices/ApiResponse.java
+++ src/main/java/com/vaghani/project/ridesharing/ridesharingapp/advices/ApiResponse.java
@@ -1,9 +1,7 @@
 package com.vaghani.project.ridesharing.ridesharingapp.advices;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
